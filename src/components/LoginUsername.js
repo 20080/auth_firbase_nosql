c43021f432ef21a267f1e9b2e9edf63fa62c6181
20080 <surajkumar2091@gmail.com>
2021-03-12 15:13:56 +0530

    struct

---
+++ src/components/LoginUsername.js
@@ -0,0 +1,14 @@
+const LoginUsername = ()=>{
+    return (
+        <div className={'FromCenter'} >
+            <form >
+                <label>Email</label>
+                <input type={'email'}/>
+                <label>Password</label>
+                <input type={'password'}/>
+            </form>
+        </div>
+    )
+}
+
+export default LoginUsername
